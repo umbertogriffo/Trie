b98924baf9ee16761be03507a5c209a1eedd5415
Umberto Griffo <umberto.griffo@gmail.com>
2017-02-21 10:08:24 +0100

    Performance test

--- Tries/src/main/java/Algo/Tries/App.java
+++ Tries/src/main/java/Algo/Tries/App.java
@@ -8,27 +8,41 @@ import java.io.IOException;
 public class App {
 	public static void main(String[] args) {
 
-		File fileName = new File("Input/testCase5");
-		
+		File fileName = new File("Input/testCase2");
+		// Calculate Elapsed / Execute Time
+		long startTime = System.currentTimeMillis();
 		// this is a case sensitive trie
 		Trie trie = new Trie(true);
 
 		try (BufferedReader br = new BufferedReader(new FileReader(fileName))) {
 			br.readLine();
 			for (String line; (line = br.readLine()) != null;) {
-				System.out.println(line);
+				//System.out.println(line);
 				String[] in = line.split(" ");
 				String op = in[0];
 				String contact = in[1];
 				if (op.equals("add")) {
 					trie.add(contact);
 				} else if (op.equals("find")) {
-					System.out.println(trie.countWordStartsWith(contact));
+					trie.countWordStartsWith(contact);
+					//System.out.println("Number Of Words Starts with 'ma': "+trie.countWordStartsWith(contact));
 				}
 			}
 
-			trie.show();
-			System.out.println(trie.getNumberOfWords());
+			//trie.show();
+			System.out.println("Number Of Words: "+trie.getNumberOfWords());
+			long stopTime = System.currentTimeMillis();
+			long elapsedTime = stopTime - startTime;
+			System.out.println('\n');
+			System.out.println("Elapsed / Execute Time");
+			System.out.println('\n');
+			System.out.println("Elapsed Time in milliseconds: " + elapsedTime);
+			System.out.println('\n');
+			System.out.println("Elapsed Time in seconds: " + (elapsedTime / 1000) % 60);
+			System.out.println('\n');
+			System.out.println("Elapsed Time in minutes: " + ((elapsedTime / (1000 * 60)) % 60));
+			System.out.println('\n');
+			System.out.println("Elapsed Time in hours: " + ((elapsedTime / (1000 * 60 * 60)) % 24));
 
 		} catch (IOException e) {
 			e.printStackTrace();
